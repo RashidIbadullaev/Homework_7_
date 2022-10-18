b4936d38e99ed07614637c2b2336c7f9a2ed5e8b
RashidIbadullaev <redmi604@mail.ru>
2022-10-18 09:22:37 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,13 @@
+public class Hero implements HavingSuperAbility {
+
+    public int health;
+
+    public int damage;
+
+    public String typeSuperPower;
+
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+
+    }
+}
